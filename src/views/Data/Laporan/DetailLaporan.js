b701b2c47b2ec4a8c2402085fbaab892a55995da
Heri Hakim Setiawan <hhakimsetiawan@gmail.com>
2019-09-09 09:06:59 +0700

    lapor client first commit

---
+++ src/views/Data/Laporan/DetailLaporan.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class DetailLaporan extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props.data);
+  }
+
+  render() {
+    return <div>tes detail</div>;
+  }
+}
+
+export default DetailLaporan;
